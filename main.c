33b837ce1616a795b768c86a33eda73d64e13714
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2022-04-18 22:27:54 +0000

    Verifica corretude e desempenho

--- main.c
+++ main.c
@@ -1,16 +1,14 @@
 /* Disciplina: Computacao Concorrente */
-/* Prof.: Silvana Rossetto */
-/* Módulo 1 - Laboratório: 1 */
-/* Codigo: "Hello World" usando threads em C e a funcao que espera as threads terminarem */
 
 #include <stdio.h>
 #include <stdlib.h> 
 #include <pthread.h>
+#include <time.h>
 
-#define NTHREADS  2 //total de threads a serem criadas
+#define NTHREADS 2 //total de threads a serem criadas
 #define DATASIZE 10000 // tamanho da coleção de dados
 
-const int dataVector[DATASIZE]; // vetor de dados
+int dataVector[DATASIZE]; // vetor de dados
 int resultVector[DATASIZE]; // vetor dos resultados
 
 //cria a estrutura de dados para armazenar os argumentos da thread
@@ -37,6 +35,7 @@ void *square (void *arg) {
   for(int i = startBound; i<endBound; i++){
     resultVector[i] = dataVector[i]*dataVector[i];
   }
+  printf("--computou o bloco de intervalo [%d,%d]\n",startBound,endBound-1);
 
   free(arg); //aqui pode liberar a alocacao feita na main
   
@@ -45,10 +44,22 @@ void *square (void *arg) {
 
 //funcao principal do programa
 int main() {
+
+  // inicializa o vetor de dados
+  srand((unsigned) time(NULL));
+  for(int i = 0; i<DATASIZE; i++){
+    dataVector[i] = rand()%1000;
+  }
+  
   pthread_t tid_sistema[NTHREADS]; //identificadores das threads no sistema
   int thread; //variavel auxiliar
   t_Args *arg; //receberá os argumentos para a thread
 
+  //inicia a contagem de tempo
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+  //cria as threads
   for(thread=0; thread<NTHREADS; thread++) {
     printf("--Aloca e preenche argumentos para thread %d\n", thread+1);
     arg = malloc(sizeof(t_Args));
@@ -70,7 +81,25 @@ int main() {
          printf("--ERRO: pthread_join() \n"); exit(-1); 
     } 
   }
+  // termina a contagem de tempo após a função de sincronização bloqueante
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+  unsigned int delta_s = (end.tv_sec - start.tv_sec);
+  long int delta_ns = (end.tv_nsec - start.tv_nsec);
+  if((end.tv_nsec - start.tv_nsec)<0){
+    delta_ns += 1000000000;
+    delta_s--;
+  }
+  printf("\nO processamento concorrente demorou %d.%.9ld segundos.\n", delta_s, delta_ns);
 
+  // Verifica a corretude do código
+  for(int i = 0; i<DATASIZE; i++){
+    if(resultVector[i] != dataVector[i]*dataVector[i]){
+      printf("\n falhou ao verificar corretude.");
+      exit(-1);
+    }
+  }
+  printf("\nverificou corretude com sucesso.\n");
+  
   printf("--Thread principal terminou\n");
   pthread_exit(NULL);
 }
