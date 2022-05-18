2081b4d408e6e13250c7dbc63dbd76b23b0d546f
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2022-05-18 19:31:08 +0000

    Finishing work

--- main.c
+++ main.c
@@ -1,166 +1,111 @@
-/*Lab-4
-  Processa primos*/
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include "timer.h"
+/*
+Lab-5
+Disciplina: Computacao Concorrente 
+*/
 
-int *entrada; //matriz de entrada
-float *saida; //matriz de saida
-float *result; // matriz para verificar a corretude
-int nthreads; //numero de threads
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int global_iter = 0; //iterador global
-pthread_mutex_t mutex; //variavel de lock para exclusao mutua
+#define NTHREADS  5
 
 typedef struct{
-   int id; //identificador do elemento que a thread ira processar
-   int dim; //dimensao das estruturas de entrada
+  int id; //identificador do elemento que a thread ira processar
 } tArgs;
 
-int ehPrimo(int n){
-  if(n <= 1)
-    return 0;
-  if(n == 2)
-    return 1;
-  if(n%2 == 0)
-    return 0;
-  for(int i = 3; i < sqrt(n)+1; i+=2){
-    if(n%i == 0)
-      return 0;
-  }
-  return 1;
+/* Variaveis globais */
+int primeiro_contador = 0;
+int ultimo_contador = 0;
+pthread_mutex_t primeiro_contador_mutex;
+pthread_mutex_t ultimo_contador_mutex;
+pthread_cond_t meio_cond;
+pthread_cond_t ultimo_cond;
+
+
+/* Thread 5 */
+void * inicio() {
+  printf("Seja bem-vindo!\n");
+
+  pthread_mutex_lock(&primeiro_contador_mutex);
+  primeiro_contador++;
+  pthread_mutex_unlock(&primeiro_contador_mutex);
+
+  pthread_mutex_lock(&ultimo_contador_mutex);
+  pthread_cond_broadcast(&meio_cond);
+  pthread_mutex_unlock(&ultimo_contador_mutex);
+
+  pthread_exit(NULL);
 }
 
-//funcao que as threads executarao
-void * tarefa(void *arg) {
-   tArgs *args = (tArgs*) arg;
-   //printf("Thread %d\n", args->id);
-  
-  // inpicio do trecho crítico com mutex
-  pthread_mutex_lock(&mutex);
-  int i = global_iter;
-  global_iter++;
-  pthread_mutex_unlock(&mutex);
-  // fim do trecho crítico
-  while(i<args->dim){
-    if (ehPrimo(entrada[i]))
-      saida[i] = sqrt(entrada[i]);
-    else
-      saida[i] = entrada[i];
+/* Thread 2, 3 e 4 */
+void * meio(void *arg) {
+  tArgs *args = (tArgs*) arg;
+  pthread_mutex_lock(&primeiro_contador_mutex);
+  if (primeiro_contador == 0){
+      pthread_cond_wait(&meio_cond, &primeiro_contador_mutex);
+  }  
+  pthread_mutex_unlock(&primeiro_contador_mutex);
 
-    // inpicio do trecho crítico com mutex
-    pthread_mutex_lock(&mutex);
-    i = global_iter;
-    global_iter++;
-    pthread_mutex_unlock(&mutex);
-    // fim do trecho crítico
+  if(args->id == 1)
+    printf("Fique a vontade.\n");
+  else if(args->id == 2)
+    printf("Sente-se por favor.\n");
+  else
+    printf("Aceita um copo d’agua?.\n");
+  
+  
+  pthread_mutex_lock(&ultimo_contador_mutex);
+  ultimo_contador++;
+  if (ultimo_contador == 3) { 
+      pthread_cond_signal(&ultimo_cond);
   }
+  pthread_mutex_unlock(&ultimo_contador_mutex); 
   
   pthread_exit(NULL);
-  //return NULL;
 }
 
-//fluxo principal
-int main(int argc, char* argv[]) {
-   int dim; //dimensao da matriz de entrada
-   pthread_t *tid; //identificadores das threads no sistema
-   tArgs *args; //identificadores locais das threads e dimensao
-   double inicio, fim, delta;
+/* Thread 1 */
+void * fim() {
+  pthread_mutex_lock(&ultimo_contador_mutex);
+  if (ultimo_contador < 3) { 
+     pthread_cond_wait(&ultimo_cond, &ultimo_contador_mutex);
+  }
+  pthread_mutex_unlock(&ultimo_contador_mutex); 
+  printf("Volte sempre!\n");
+  pthread_exit(NULL);
+}
+
+/* Funcao principal */
+int main(int argc, char *argv[]) {
+  int i; 
+  pthread_t threads[NTHREADS];
+
+  /* Inicilaiza o mutex (lock de exclusao mutua) e a variavel de condicao */
+  pthread_mutex_init(&primeiro_contador_mutex, NULL);
+  pthread_mutex_init(&ultimo_contador_mutex, NULL);
+  pthread_cond_init (&meio_cond, NULL);
+  pthread_cond_init (&ultimo_cond, NULL);
+
+  tArgs *args = (tArgs*) malloc(sizeof(tArgs)*3);
+  if(args==NULL) {puts("ERRO--malloc"); return 2;}
    
-   GET_TIME(inicio);
-   //leitura e avaliacao dos parametros de entrada
-   if(argc<3) {
-      printf("Digite: %s <dimensao da matriz> <numero de threads>\n", argv[0]);
-      return 1;
-   }
-   dim = atoi(argv[1]);
-   nthreads = atoi(argv[2]);
-   if (nthreads > dim) nthreads=dim;
 
-   //alocacao de memoria para as estruturas de dados
-   entrada = (int *) malloc(sizeof(int) * dim);
-   if (entrada == NULL) {printf("ERRO--malloc\n"); return 2;}
-   saida = (float *) malloc(sizeof(float) * dim);
-   if (saida == NULL) {printf("ERRO--malloc\n"); return 2;}
-   result = (float *) malloc(sizeof(float) * dim);
-   if (result == NULL) {printf("ERRO--malloc\n"); return 2;}
-
-   srand((unsigned) time(NULL));
-   //inicializacao das estruturas de dados de entrada e saida
-   for(int i=0; i<dim; i++) {
-    entrada[i] = rand(); 
-    saida[i] = 0;
-    result[i] = 0;
-   }
-
-   //--inicilaiza o mutex (lock de exclusao mutua)
-   pthread_mutex_init(&mutex, NULL);
-  
-   GET_TIME(fim);
-   delta = fim - inicio;
-   //printf("Tempo inicializacao:%lf\n", delta);
-  
-   //início da tarefa
-   GET_TIME(inicio);
-   //alocacao das estruturas
-   tid = (pthread_t*) malloc(sizeof(pthread_t)*nthreads);
-   if(tid==NULL) {puts("ERRO--malloc"); return 2;}
-   args = (tArgs*) malloc(sizeof(tArgs)*nthreads);
-   if(args==NULL) {puts("ERRO--malloc"); return 2;}
-   //criacao das threads
-   for(int i=0; i<nthreads; i++) {
-      (args+i)->id = i;
-      (args+i)->dim = dim;
-      if(pthread_create(tid+i, NULL, tarefa, (void*) (args+i))){
-         puts("ERRO--pthread_create"); return 3;
-      }
-   } 
-   //espera pelo termino da threads
-   for(int i=0; i<nthreads; i++) {
-      pthread_join(*(tid+i), NULL);
-   }
-   GET_TIME(fim);   
-   delta = fim - inicio;
-   printf("Tempo concorrente: %lf\n", delta);
-
-  //calculando o resultado sequencial
-  GET_TIME(inicio);
-  for(int i=0; i<dim; i++) {
-    if (ehPrimo(entrada[i]))
-      result[i] = sqrt(entrada[i]);
-    else
-      result[i] = entrada[i];
+  /* Cria as threads */
+  pthread_create(&threads[0], NULL, fim, NULL);
+  for(int i=1; i <= 3; i++){
+    (args+i)->id = i;
+    pthread_create(&threads[i], NULL, meio, (void*) (args+i)); 
   }
-  GET_TIME(fim);   
-  delta = fim - inicio;
-  printf("Tempo sequencial: %lf\n", delta);
-  for(int i=0; i<dim; i++) {
-    if(result[i] != saida[i]){
-      printf("Falhou no teste de corretude\n");
-      free(entrada);
-      free(saida);
-      free(result);
-      free(args);
-      free(tid);
-      pthread_mutex_destroy(&mutex);
-      exit(-1);
-    }
+  pthread_create(&threads[4], NULL, inicio, NULL);
+
+  /* Espera todas as threads completarem */
+  for (i = 0; i < NTHREADS; i++) {
+  pthread_join(threads[i], NULL);
   }
-  //liberacao da memoria
-  GET_TIME(inicio);
-  free(entrada);
-  free(saida);
-  free(result);
-  free(args);
-  free(tid);
-  pthread_mutex_destroy(&mutex);
-  GET_TIME(fim);   
-  delta = fim - inicio;
-  //printf("Tempo finalizacao:%lf\n", delta);
 
-  printf("Passou no teste de corretude\n");
-
-  return 0;
+  /* Desaloca variaveis e termina */
+  pthread_mutex_destroy(&primeiro_contador_mutex);
+  pthread_mutex_destroy(&ultimo_contador_mutex);
+  pthread_cond_destroy(&meio_cond);
+  pthread_cond_destroy(&ultimo_cond);
 }
