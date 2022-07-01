695875e5e0785cad6ee0d3caa8e68f9cb8ca602e
Renan Mendanha Alvarino <renanma@dcc.ufrj.br>
2022-07-01 03:53:09 +0000

    permitindo execução paralela das threads do meio

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ typedef struct{
 /* semáforos */
 sem_t meio_cond;
 sem_t ultimo_cond;
+sem_t semBin;
 // contador para o último semáforo
 int ultimo_contador=0;
 
@@ -24,8 +25,9 @@ int ultimo_contador=0;
 void * inicio() {
   printf("Seja bem-vindo!\n");
 
-  // desbloqueia uma thread do tipo meio
-  sem_post(&meio_cond);
+  // desbloqueia 3 threads do tipo meio
+  for(int i=0;i<3;i++)
+    sem_post(&meio_cond);
 
   pthread_exit(NULL);
 }
@@ -44,14 +46,17 @@ void * meio(void *arg) {
   else
     printf("Aceita um copo d’agua?.\n");
 
+  // garante exclusão mútua
+  sem_wait(&semBin);
   // soma no contador que dispara a última thread 
   ultimo_contador++;
+  // fim da seção crítica
+  sem_post(&semBin);
+
+  // verifica se é a última thread do tipo meio
   if (ultimo_contador == 3)
     // libera uma thread do tipo fim
     sem_post(&ultimo_cond);
-  else
-    // libera uma thread do tipo meio
-    sem_post(&meio_cond);
   
   pthread_exit(NULL);
 }
@@ -71,9 +76,13 @@ int main(int argc, char *argv[]) {
   int i; 
   pthread_t threads[NTHREADS];
 
-  /* Inicilaiza o semáforo */
+  /* Inicilaiza os semáforos */
+  // threads do meio
   sem_init(&meio_cond, 0, 0);
+  //thread do fim
   sem_init(&ultimo_cond, 0, 0);
+  // semáforo binário para exclusão mútua
+  sem_init(&semBin, 0, 1);
 
   tArgs *args = (tArgs*) malloc(sizeof(tArgs)*3);
   if(args==NULL) {puts("ERRO--malloc"); return 2;}
@@ -96,5 +105,6 @@ int main(int argc, char *argv[]) {
   free(args);
   sem_destroy(&meio_cond);
   sem_destroy(&ultimo_cond);  
+  sem_destroy(&semBin);
   pthread_exit(NULL);
 }
